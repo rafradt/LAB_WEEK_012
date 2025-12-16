d21ac360dd9b05f27bbeaf6ea3bd36afe6fce9dd
rafradt <rafi.raditya165@gmail.com>
2025-12-16 21:44:27 +0700

    Commit No. 2 – Building app with coroutines

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,15 @@ plugins {
 
 android {
     namespace = "com.example.test_lab_week_12"
-    compileSdk = 36
+
+    // WAJIB karena RecyclerView 1.4.0
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.test_lab_week_12"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
+
         versionCode = 1
         versionName = "1.0"
 
@@ -27,32 +30,50 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
+    }
+
+    buildFeatures {
+        viewBinding = true
     }
 }
 
 dependencies {
 
-    implementation(libs.androidx.recyclerview)
-    implementation(libs.glide)
-    implementation(libs.retrofit)
-    implementation(libs.converter.moshi)
-    implementation(libs.kotlinx.coroutines.core)
-    implementation(libs.kotlinx.coroutines.android)
-    kapt(libs.moshi.kotlin.codegen)
-    implementation(libs.androidx.lifecycle.livedata.ktx)
-    implementation(libs.androidx.lifecycle.viewmodel.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
+    // Core Android
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.recyclerview)
+
+    // Lifecycle
+    implementation(libs.lifecycle.viewmodel.ktx)
+    implementation(libs.lifecycle.livedata.ktx)
+    implementation(libs.lifecycle.runtime.ktx)
+
+    // Coroutines
+    implementation(libs.coroutines.core)
+    implementation(libs.coroutines.android)
+
+    // Networking
+    implementation(libs.retrofit)
+    implementation(libs.converter.moshi)
+
+    // Moshi
+    implementation(libs.moshi)
+    implementation(libs.moshi.kotlin)
+    kapt(libs.moshi.codegen)
+
+    // Test
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/test_lab_week_12/MovieRepository.kt
+++ app/src/main/java/com/example/test_lab_week_12/MovieRepository.kt
@@ -1,7 +1,26 @@
 package com.example.test_lab_week_12
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.test_lab_week_12.api.MovieService
+import com.example.test_lab_week_12.model.Movie
 
 class MovieRepository(private val movieService: MovieService) {
 
+    private val apiKey = "5f2a6337319819e2fe92773701c79f4d"
+
+    private val movieLiveData = MutableLiveData<List<Movie>>()
+    val movies: LiveData<List<Movie>> get() = movieLiveData
+
+    private val errorLiveData = MutableLiveData<String>()
+    val error: LiveData<String> get() = errorLiveData
+
+    suspend fun fetchMovies() {
+        try {
+            val popularMovies = movieService.getPopularMovies(apiKey)
+            movieLiveData.postValue(popularMovies.results)
+        } catch (e: Exception) {
+            errorLiveData.postValue("An error occurred: ${e.message}")
+        }
+    }
 }
